c6ef6334ee41bb6e279fbca8960c1419d6c68bd4
Agueda <gmz.cambronero@gmail.com>
2020-09-29 21:39:55 +0200

    Working on multiple card

---
+++ Assets/Scripts/ScriptableObjects/Multiple.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Multiple", menuName = "Card/Multiple", order = 1)]
+
+public class Multiple : Card
+{
+    public Choice choice1; //Top
+    public Choice choice2;
+    public Choice choice3;
+    public Choice choice4; //Bottom
+    public Card nextCardIfRight;
+    public Card nextCardIfLeft;
+}
