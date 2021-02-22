656783334b178f8b588a2f692619530a228a92e4
Agueda <gmz.cambronero@gmail.com>
2021-02-22 09:50:05 +0100

    Add prototype 3d models and change grid

--- Assets/Scripts/CityBuilder/Board.cs
+++ Assets/Scripts/CityBuilder/Board.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    private Building[,] buildings = new Building[100, 100];
+    private int cellSize = 2;
+    private float boardHeight, boardWidth;
+    private int numCells;
+    private Building[,] buildings;
+
+    void Start()
+    {
+        boardWidth = transform.localScale.x;
+        boardHeight = transform.localScale.z;
+
+        numCells = (int)boardWidth / cellSize; //celdas cuadradas
+        buildings = new Building[numCells, numCells];
+
+       // print("board height = " + boardHeight + " board width = " + boardWidth + " num celdas = " + numCells);
+    }
 
     public void AddBuilding(GameObject building, Vector3 position)
     {
@@ -26,6 +40,23 @@ public class Board : MonoBehaviour
 
     public Vector3 CalculateGridPosition(Vector3 position)
     {
-        return new Vector3(Mathf.Round(position.x), .5f, Mathf.Round(position.z));
+        int xCell = Mathf.RoundToInt(position.x / cellSize);
+        float yCell = .5f;
+        int zCell = Mathf.RoundToInt(position.z / cellSize);
+
+        return new Vector3(xCell*cellSize, yCell, zCell*cellSize);
     }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.white;
+    //    for (float x = 0; x < 30; x += CELLSIZE)
+    //    {
+    //        for (float z = 0; z < 30; z += CELLSIZE)
+    //        {
+    //            var point = CalculateGridPosition(new Vector3(x, 0.9f, z));
+    //            Gizmos.DrawSphere(point, 0.1f);
+    //        }
+    //    }
+    //}
 }
