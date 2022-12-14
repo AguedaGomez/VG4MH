0a42bfff0dca91b808fcea4b52bcfce59f4ee68a
Águeda Gómez <gmz.cambronero@gmail.com>
2022-12-14 15:58:41 +0100

    Active story checkpoints

--- Assets/TheoryBookManager.cs
+++ Assets/TheoryBookManager.cs
@@ -25,11 +25,11 @@ public class TheoryBookManager : MonoBehaviour
         foreach(EntryObject entry in bookEntries)
         {
             //Descomentar para probar toda la secuencia
-            //if (GameManager.Instance.checkPointsStory.Contains(entry.typeContent))
-            //{
-            //    entry.unlocked = true;
-            //}
-            if(entry.unlocked)
+            if (GameManager.Instance.checkPointsStory.Contains(entry.typeContent))
+            {
+                entry.unlocked = true;
+            }
+            if (entry.unlocked)
             {
                 GameObject entryObject = Instantiate(entryPrefab, mainMenu_Viewport_gameobject.transform);
                 entryObject.GetComponent<EntryDecorator>().SetUp(entry);
