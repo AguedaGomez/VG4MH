538f12bdd91b8775f4adca12ef8e209bcca306fe
Águeda Gómez <gmz.cambronero@gmail.com>
2022-12-20 17:51:42 +0100

    Add MainMenu in the flow

---
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Button playButton;
+    public InputField idInputField;
+    public Card tutorial;
+
+    private string ID = "123456";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void Login()
+    {
+        
+    }
+    public void StartGame ()
+    {
+        if (SaveObject.Instance.date == "")
+        {
+            GameManager.Instance.currentCard = tutorial;
+
+            SceneManager.LoadScene("DecisionMakingGame");
+        } else
+        {
+            //SceneManager.LoadScene("CityBuilder");
+            GameManager.Instance.SaveGame();
+            GameManager.Instance.LoadGame();
+        }
+    }
+}
